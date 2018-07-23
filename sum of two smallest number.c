1d1ca9be3e542351af9583b04a448260ac0c890a
NESAN Y <NESAN Y@NESAN>
2018-07-23 22:45:40 +0530

    find the sum of two numbers

---
+++ sum of two smallest number.c
@@ -0,0 +1,31 @@
+
+find sum of two smallest number:
+input
+4
+4 7 3 8
+output
+7
+c-program
+#include<stdio.h>
+void main()
+{
+    int n,a[100],i,j,temp;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("%d",a[0]+a[1]);
+}
